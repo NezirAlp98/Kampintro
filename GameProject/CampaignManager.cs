7a6d6bf327bdc7d08b6f6dfbf5862957235d8b7e
nezir alp arslan <nezirarslan1234@gmail.com>
2021-02-05 19:58:56 +0300

    5 şubat sonu

---
+++ GameProject/CampaignManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class CampaignManager:IOrderManager
+    {
+        public void Add(Gamer gamer)
+        {
+            Console.WriteLine("Kampanya oluşturuldu.");
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kampanya silindi.");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kampanya güncellendi.");
+        }
+    }
+}
---
+++ SQLQuery6.sql
@@ -0,0 +1,34 @@
+﻿--Select
+--ANSII
+Select ContactName Adi,CompanyName Sirketadi,City Sehir from Customers
+
+Select * from Customers where City='London'
+
+--case insensitive
+select * from Products where CategoryID=1 or CategoryID=3
+
+select * from Products where CategoryID=1 and UnitPrice>=10
+
+select * from Products where CategoryID=1 order by UnitPrice desc    --ascending --descending
+
+select count(*) Adet from Products where CategoryID=2
+
+
+select CategoryID,count(*) Adet from Products group by CategoryID
+
+select CategoryID,count(*) Adet from Products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID,Products.ProductName,Products.UnitPrice,Categories.CategoryName
+ from Products inner join Categories on Products.CategoryID=Categories.CategoryID
+where Products.UnitPrice>10
+
+--DTO Data Transformation Object
+
+select * from Products p left join [Order Details] od
+on p.ProductID=od.ProductID
+inner join Orders o on o.OrderID=od.OrderID 
+
+select * from Customers c left join Orders o
+on c.CustomerID=o.CustomerID
+where o.CustomerID is null
+
